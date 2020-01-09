26b9b07d2f43e4cb6e559c42adc1b92e69b680f6
Ted M. Young <tedyoung@gmail.com>
2020-01-09 15:50:05 -0800

    Game Master view of cards now has correct coloring.

--- frontend/src/components/PlayingCardsMiniView.vue
+++ frontend/src/components/PlayingCardsMiniView.vue
@@ -2,8 +2,13 @@
   <div class="w-2/3 mb-4">
     <div v-for="(card, index) in cards"
          :key="index"
-         class="border-l-8 border-yellow-400 bg-yellow-200 py-2 pl-2 pr-4 mb-1">
-      <p>{{ card.title }} ({{card.id}})</p>
+    >
+      <div
+        class="border-l-8 py-2 pl-2 pr-4 mb-1"
+        :class="cardColors(card)"
+      >
+        {{ card.title }} ({{card.id}})
+      </div>
     </div>
   </div>
 </template>
@@ -14,6 +19,23 @@
       cards: {
         type: Array,
         required: true
+      }
+    },
+    methods: {
+      cardColors(card) {
+        return this.titleToColor[card.title];
+      }
+    },
+    data() {
+      return {
+        titleToColor: {
+          'write code': 'border-teal-400 bg-teal-200',
+          'code smaller': 'border-teal-400 bg-teal-200',
+          'predict': 'border-yellow-400 bg-yellow-200',
+          'code bloat': 'border-red-400 bg-red-200',
+          "can't assert": 'border-red-400 bg-red-200',
+          'refactor': 'border-indigo-400 bg-indigo-200'
+        }
       }
     }
   }
