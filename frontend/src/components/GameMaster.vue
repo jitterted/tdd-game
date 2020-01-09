5fe2dc91c0ac986d0a1da1ddb884c40cc32c6b1d
Ted M. Young <tedyoung@gmail.com>
2020-01-09 15:14:23 -0800

    Made player one's cards dynamic (but hard-coded player array access).

--- frontend/src/components/GameMaster.vue
+++ frontend/src/components/GameMaster.vue
@@ -8,40 +8,23 @@
       <div class="w-1/2 border shadow-lg">
         <div class="bg-blue-700 text-white uppercase font-bold p-2 px-12 text-center mb-2">
           <span class="block tracking-widest text-sm">Player One</span>
-          <span class="block tracking-wider text-2xl">Ted</span>
+          <span class="block tracking-wider text-2xl">{{ game.players[0].name }}</span>
         </div>
-        <div class="flex w-11/12 mx-auto flex-wrap text-lg">
+        <div class="flex w-11/12 mx-auto flex-wrap">
           <div class="w-1/3 border-t-2 border-gray-500">IN PLAY</div>
           <div class="w-2/3 mb-4">
-            <div class="border-l-8 border-yellow-400 bg-yellow-200 py-2 pl-2 pr-4 mb-1">
-              <p>Predict</p>
-            </div>
-            <div class="border-l-8 border-blue-400 bg-blue-200 py-2 pl-2 pr-4 mb-1">
-              <p>Write Code</p>
-            </div>
-            <div class="border-l-8 border-blue-400 bg-blue-200 py-2 pl-2 pr-4 mb-1">
-              <p>Code Smaller</p>
-            </div>
-            <div class="border-l-8 border-red-400 bg-red-200 py-2 pl-2 pr-4 mb-1">
-              <p>Can't Assert</p>
+            <div v-for="(card, index) in game.players[0].inPlay.cards"
+                 :key="index"
+                 class="border-l-8 border-yellow-400 bg-yellow-200 py-2 pl-2 pr-4 mb-1">
+              <p>{{ card.title }} ({{card.id}})</p>
             </div>
           </div>
           <div class="w-1/3 border-t-2 border-gray-500">HAND</div>
           <div class="w-2/3">
-            <div class="border-l-8 border-yellow-400 bg-yellow-200 py-2 pl-2 pr-4 mb-1">
-              <p>Predict</p>
-            </div>
-            <div class="border-l-8 border-blue-400 bg-blue-200 py-2 pl-2 pr-4 mb-1">
-              <p>Code Smaller</p>
-            </div>
-            <div class="border-l-8 border-blue-400 bg-blue-200 py-2 pl-2 pr-4 mb-1">
-              <p>Code Smaller</p>
-            </div>
-            <div class="border-l-8 border-teal-400 bg-teal-200 py-2 pl-2 pr-4 mb-1">
-              <p>Refactor</p>
-            </div>
-            <div class="border-l-8 border-yellow-400 bg-yellow-200 py-2 pl-2 pr-4 mb-1">
-              <p>Predict</p>
+            <div v-for="(card, index) in game.players[0].hand.cards"
+                 :key="index"
+                 class="border-l-8 border-yellow-400 bg-yellow-200 py-2 pl-2 pr-4 mb-1">
+              <p>{{ card.title }} ({{card.id}})</p>
             </div>
           </div>
         </div>
@@ -49,9 +32,9 @@
       <div class="w-1/2 border shadow-lg">
         <div class="bg-green-700 text-white uppercase font-bold p-2 px-12 text-center mb-2">
           <span class="block tracking-widest text-sm">Player Two</span>
-          <span class="block tracking-wider text-2xl">Alex</span>
+          <span class="block tracking-wider text-2xl">{{ game.players[1].name }}</span>
         </div>
-        <div class="flex w-11/12 mx-auto flex-wrap text-lg">
+        <div class="flex w-11/12 mx-auto flex-wrap">
           <div class="w-1/3 border-t-2 border-gray-500">IN PLAY</div>
           <div class="w-2/3 mb-4">
             <div class="border-l-8 border-yellow-400 bg-yellow-200 py-2 pl-2 pr-4 mb-1">
