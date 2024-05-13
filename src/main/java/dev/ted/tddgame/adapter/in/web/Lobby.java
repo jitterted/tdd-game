dc65a58ea781031e5cf44020b2622c7455a82ffd
Ted M. Young <tedyoung@gmail.com>
2024-05-13 13:25:08 -0700

    Lobby now knows about GameViews to display

--- src/main/java/dev/ted/tddgame/adapter/in/web/Lobby.java
+++ src/main/java/dev/ted/tddgame/adapter/in/web/Lobby.java
@@ -1,5 +1,6 @@
 package dev.ted.tddgame.adapter.in.web;
 
+import dev.ted.tddgame.domain.GameView;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,9 +8,28 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.security.Principal;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class Lobby {
+
+    private final List<GameView> gameViews = new ArrayList<>();
+
+    public Lobby() {
+    }
+
+    public Lobby(List<GameView> gameViews) {
+        this.gameViews.addAll(gameViews);
+    }
+
+    public static Lobby createNull() {
+        return new Lobby();
+    }
+
+    public static Lobby createNull(GameView gameView) {
+        return new Lobby(List.of(gameView));
+    }
 
     @GetMapping("/")
     public String redirectToLobby() {
@@ -19,11 +39,13 @@ public class Lobby {
     @GetMapping("/lobby")
     public String showLobby(Principal principal, Model model) {
         model.addAttribute("personName", principal.getName());
-        return "lobby";
+        model.addAttribute("gameViews", gameViews);
+        return gameViews.isEmpty() ? "no-game-lobby" : "lobby";
     }
 
     @PostMapping("/games")
-    public String hostNewGame(Principal principal, @RequestParam("newGameName") String newGameName) {
+    public String hostNewGame(Principal principal,
+                              @RequestParam("newGameName") String newGameName) {
         return "redirect:/lobby";
     }
 
--- src/test/java/dev/ted/tddgame/adapter/in/web/LobbyTest.java
+++ src/test/java/dev/ted/tddgame/adapter/in/web/LobbyTest.java
@@ -1,7 +1,52 @@
 package dev.ted.tddgame.adapter.in.web;
 
+import dev.ted.tddgame.domain.GameView;
+import org.assertj.core.api.InstanceOfAssertFactories;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.ui.ConcurrentModel;
+
+import java.security.Principal;
+
+import static org.assertj.core.api.Assertions.*;
+
 class LobbyTest {
 
+    private static final Principal DUMMY_PRINCIPAL = () -> "Ted";
+
+    @Test
+    void noGamesLobbyViewWhenNoGamesExist() {
+        Lobby lobby = Lobby.createNull();
+
+        String viewName = lobby.showLobby(DUMMY_PRINCIPAL, new ConcurrentModel());
+
+        assertThat(viewName)
+                .isEqualTo("no-game-lobby");
+    }
+
+    @Test
+    void lobbyViewShowsGamesWhenGamesExist() {
+        Lobby lobby = Lobby.createNull(new GameView());
+
+        ConcurrentModel model = new ConcurrentModel();
+        String viewName = lobby.showLobby(DUMMY_PRINCIPAL, model);
+
+        assertThat(viewName)
+                .isEqualTo("lobby");
+
+        assertThat(model.asMap())
+                .extracting("gameViews")
+                .asInstanceOf(InstanceOfAssertFactories.list(GameView.class))
+                .containsExactly(new GameView());
+    }
+
+    @Test
+    @Disabled("Until showLobby returns existing games to join")
+    void newGameWithHandleCreatedUponHostNewGame() {
+        Lobby lobby = Lobby.createNull();
+
+        lobby.hostNewGame(DUMMY_PRINCIPAL, "game name");
 
 
+    }
 }
