5e7770bad1c70038f300a31e70a0f49c05cec14b
Ted M. Young <tedyoung@gmail.com>
2024-05-29 12:44:40 -0700

    Added shuffling to the Deck, with a Nullable option for tests that uses the passed-in cards as what is returned from the embedded test shuffler.

--- src/main/java/dev/ted/tddgame/domain/Deck.java
+++ src/main/java/dev/ted/tddgame/domain/Deck.java
@@ -1,16 +1,30 @@
 package dev.ted.tddgame.domain;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
 public class Deck<CARD> {
     private final Queue<CARD> drawPile = new LinkedList<>();
-    private final List<CARD> discardPile = new ArrayList<>();
+    private final Shuffler<CARD> shuffler;
+    private List<CARD> discardPile;
 
-    public Deck(List<CARD> cards) {
-        discardPile.addAll(cards);
+    // for production usage, uses random shuffler
+    public static <CARD> Deck<CARD> create(List<CARD> cards) {
+        return new Deck<>(cards, new RandomShuffler<>());
+    }
+
+    private Deck(List<CARD> cards, Shuffler<CARD> shuffler) {
+        discardPile = new ArrayList<>(cards);
+        this.shuffler = shuffler;
+    }
+
+    public static Deck<ActionCard> createForTest(ActionCard... actionCards) {
+        return new Deck<>(Collections.emptyList(),
+                          _ -> new ArrayList<>(Arrays.asList(actionCards)));
     }
 
     public CARD draw() {
@@ -25,7 +39,23 @@ public class Deck<CARD> {
     }
 
     private void replenishDrawPileFromDiscardPile() {
+        discardPile = shuffler.shuffleCards(discardPile);
         drawPile.addAll(discardPile);
         discardPile.clear();
+    }
+
+    // -- EMBEDDED STUB for Nullable Shuffler --
+
+    private static class RandomShuffler<CARD> implements Shuffler<CARD> {
+        @Override
+        public List<CARD> shuffleCards(List<CARD> discardPile) {
+            List<CARD> toBeShuffled = new ArrayList<>(discardPile);
+            Collections.shuffle(toBeShuffled);
+            return toBeShuffled;
+        }
+    }
+
+    private interface Shuffler<CARD> {
+        List<CARD> shuffleCards(List<CARD> discardPile);
     }
 }
--- src/test/java/dev/ted/tddgame/domain/DeckTest.java
+++ src/test/java/dev/ted/tddgame/domain/DeckTest.java
@@ -17,7 +17,7 @@ class DeckTest {
             List<ActionCard> actionCards = List.of(
                     ActionCard.PREDICT,
                     ActionCard.WRITE_CODE);
-            Deck<ActionCard> deck = new Deck<>(actionCards);
+            Deck<ActionCard> deck = Deck.create(actionCards);
 
             assertThat(deck.isDrawPileEmpty())
                     .as("Draw Pile should be empty when Deck is created")
@@ -25,13 +25,13 @@ class DeckTest {
         }
 
         @Test
-        void canDrawFullSetOfCardsAfterShuffleTriggeredByDraw() throws Exception {
-            List<ActionCard> actionCards = List.of(
+        void canDrawFullSetOfCardsAfterReplenishTriggeredByDraw() throws Exception {
+            Deck<ActionCard> deck = Deck.createForTest(
                     ActionCard.PREDICT,
                     ActionCard.WRITE_CODE,
                     ActionCard.REFACTOR,
-                    ActionCard.LESS_CODE);
-            Deck<ActionCard> deck = new Deck<>(actionCards);
+                    ActionCard.LESS_CODE
+            );
 
             List<ActionCard> drawnCards = drawCards(deck, 4);
 
@@ -44,7 +44,40 @@ class DeckTest {
             assertThat(deck.isDrawPileEmpty())
                     .isTrue();
         }
+
+        @Test
+        void drawnCardsAreFromReplenishedShuffledDiscardPile() {
+            List<ActionCard> allStandardActionCards = createStandardActionCards();
+            Deck<ActionCard> deck = Deck.create(allStandardActionCards);
+
+            List<ActionCard> drawnCards = new ArrayList<>();
+            do {
+                drawnCards.add(deck.draw());
+            } while (!deck.isDrawPileEmpty());
+
+            assertThat(drawnCards)
+                    .doesNotContainSequence(allStandardActionCards);
+
+        }
     }
+
+    private List<ActionCard> createStandardActionCards() {
+        List<ActionCard> allActionCards = new ArrayList<>();
+        addActionCards(18, ActionCard.WRITE_CODE, allActionCards);
+        addActionCards(18, ActionCard.LESS_CODE, allActionCards);
+        addActionCards(18, ActionCard.PREDICT, allActionCards);
+        addActionCards(2, ActionCard.CANT_ASSERT, allActionCards);
+        addActionCards(3, ActionCard.CODE_BLOAT, allActionCards);
+        addActionCards(4, ActionCard.REFACTOR, allActionCards);
+        return allActionCards;
+    }
+
+    private void addActionCards(int count, ActionCard actionCard, List<ActionCard> allActionCards) {
+        for (int i = 0; i < count; i++) {
+            allActionCards.add(actionCard);
+        }
+    }
+
 
     private List<ActionCard> drawCards(Deck<ActionCard> deck, int numberOfCards) {
         List<ActionCard> drawnCards = new ArrayList<>();
