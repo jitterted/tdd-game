8f154afe383b3f05a3c3f6f4b50be4a507784c92
Ted M. Young <tedyoung@gmail.com>
2024-10-29 14:37:14 -0700

    POST to "start game" endpoint now passes

--- src/test/java/dev/ted/tddgame/adapter/in/web/PlayingGameMvcTest.java
+++ src/test/java/dev/ted/tddgame/adapter/in/web/PlayingGameMvcTest.java
@@ -4,7 +4,7 @@ import dev.ted.tddgame.SecurityConfig;
 import dev.ted.tddgame.TddGameConfig;
 import dev.ted.tddgame.application.port.GameStore;
 import dev.ted.tddgame.domain.Game;
-import org.junit.jupiter.api.Disabled;
+import dev.ted.tddgame.domain.MemberId;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,14 +32,18 @@ class PlayingGameMvcTest {
     @Test
     void getToGameEndpointReturns200() throws Exception {
         gameStore.save(Game.create("Game Name", "gameHandle"));
+
         mockMvc.perform(get("/game/gameHandle"))
                .andExpect(status().isOk());
     }
 
     @Test
-    @Disabled("Until the Game is created properly with the initial set of Action Cards")
     void postToStartEndpointReturns204() throws Exception {
-        mockMvc.perform(post("/game/gameHandle/start-game"))
+        Game game = Game.create("Game to be Started", "game2start");
+        game.join(new MemberId(1L), "Player 1");
+        gameStore.save(game);
+
+        mockMvc.perform(post("/game/game2start/start-game"))
                .andExpect(status().isNoContent());
     }
 }
