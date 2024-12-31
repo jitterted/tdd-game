7aa45fc5874c0d429d781d8fb5ab8a2897ffaadf
Ted M. Young <tedyoung@gmail.com>
2024-12-31 15:57:00 -0800

    Pulled out HtmlComponent as a top-level class

---
+++ src/main/java/dev/ted/tddgame/adapter/out/websocket/HtmlComponent.java
@@ -0,0 +1,102 @@
+package dev.ted.tddgame.adapter.out.websocket;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public abstract class HtmlComponent {
+    protected final List<HtmlComponent> htmlComponent;
+
+    public HtmlComponent(HtmlComponent... htmlComponent) {
+        this.htmlComponent = List.of(htmlComponent);
+    }
+
+    protected String renderNested() {
+        return htmlComponent.stream()
+                            .map(this::render)
+                            .collect(Collectors.joining());
+    }
+
+    private String render(HtmlComponent component) {
+        return component.render()
+                        .lines()
+                        .map(s -> "    " + s + "\n")
+                        .collect(Collectors.joining());
+    }
+
+    public String render() {
+        return renderTagOpen()
+               +
+               renderNested()
+               +
+               renderTagClose();
+    }
+
+    protected abstract String renderTagClose();
+
+    protected abstract String renderTagOpen();
+
+    static class SwapComponent extends HtmlComponent {
+
+        public SwapComponent(HtmlComponent... htmlComponent) {
+            super(htmlComponent);
+        }
+
+        @Override
+        protected String renderTagOpen() {
+            return """
+                   <swap id="you" hx-swap-oob="innerHTML">
+                   """;
+        }
+
+        @Override
+        protected String renderTagClose() {
+            return "</swap>\n";
+        }
+    }
+
+    static class DivHtmlComponent extends HtmlComponent {
+
+        private final String htmlClass;
+
+        public DivHtmlComponent(String htmlClass, HtmlComponent... htmlComponents) {
+            super(htmlComponents);
+            this.htmlClass = htmlClass;
+        }
+
+        @Override
+        protected String renderTagOpen() {
+            return """
+                   <div class="%s">
+                   """.formatted(htmlClass);
+        }
+
+        @Override
+        protected String renderTagClose() {
+            return "</div>";
+        }
+
+    }
+
+    static class TextComponent extends HtmlComponent {
+
+        private final String text;
+
+        public TextComponent(String text) {
+            this.text = text;
+        }
+
+        public String render() {
+            return text;
+        }
+
+        @Override
+        protected String renderTagClose() {
+            return "";
+        }
+
+        @Override
+        protected String renderTagOpen() {
+            return "";
+        }
+    }
+}
--- src/main/java/dev/ted/tddgame/adapter/out/websocket/PlayerViewComponent.java
+++ src/main/java/dev/ted/tddgame/adapter/out/websocket/PlayerViewComponent.java
@@ -3,8 +3,6 @@ package dev.ted.tddgame.adapter.out.websocket;
 import dev.ted.tddgame.domain.ActionCard;
 import dev.ted.tddgame.domain.Player;
 
-import java.util.List;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class PlayerViewComponent {
@@ -15,121 +13,23 @@ public class PlayerViewComponent {
     }
 
     public String generateHtmlAsYou() {
-        HtmlComponent workspaceDiv = new DivHtmlComponent("workspace",
-                                                          new TextComponent("<h2>Workspace</h2>"));
-        HtmlComponent handComponent = new DivHtmlComponent("hand",
-                                                           createDivsForEach(player.hand()));
-        HtmlComponent handContainerDiv = new DivHtmlComponent("titled-container",
-                                                              new TextComponent("Your Hand"),
-                                                              handComponent);
-        return new SwapComponent(workspaceDiv, handContainerDiv)
+        HtmlComponent workspaceDiv = new HtmlComponent.DivHtmlComponent("workspace",
+                                                                        new HtmlComponent.TextComponent("<h2>Workspace</h2>"));
+        HtmlComponent handComponent = new HtmlComponent.DivHtmlComponent("hand",
+                                                                         createDivsForEach(player.hand()));
+        HtmlComponent handContainerDiv = new HtmlComponent.DivHtmlComponent("titled-container",
+                                                                            new HtmlComponent.TextComponent("Your Hand"),
+                                                                            handComponent);
+        return new HtmlComponent.SwapComponent(workspaceDiv, handContainerDiv)
                 .render();
     }
 
     static HtmlComponent[] createDivsForEach(Stream<ActionCard> actionCards) {
         return actionCards
-                .map(card -> new DivHtmlComponent("card",
-                                                  new TextComponent(card.title())))
+                .map(card -> new HtmlComponent.DivHtmlComponent("card",
+                                                                new HtmlComponent.TextComponent(card.title())))
                 .toList()
                 .toArray(new HtmlComponent[0]);
-    }
-
-    static class SwapComponent extends HtmlComponent {
-
-        public SwapComponent(HtmlComponent... htmlComponent) {
-            super(htmlComponent);
-        }
-
-        @Override
-        protected String renderTagOpen() {
-            return """
-                   <swap id="you" hx-swap-oob="innerHTML">
-                   """;
-        }
-
-        @Override
-        protected String renderTagClose() {
-            return "</swap>\n";
-        }
-    }
-
-    private static class DivHtmlComponent extends HtmlComponent {
-
-        private final String htmlClass;
-
-        public DivHtmlComponent(String htmlClass, HtmlComponent... htmlComponents) {
-            super(htmlComponents);
-            this.htmlClass = htmlClass;
-        }
-
-        @Override
-        protected String renderTagOpen() {
-            return """
-                   <div class="%s">
-                   """.formatted(htmlClass);
-        }
-
-        @Override
-        protected String renderTagClose() {
-            return "</div>";
-        }
-
-    }
-
-    private static class TextComponent extends HtmlComponent {
-
-        private final String text;
-
-        public TextComponent(String text) {
-            this.text = text;
-        }
-
-        public String render() {
-            return text;
-        }
-
-        @Override
-        protected String renderTagClose() {
-            return "";
-        }
-
-        @Override
-        protected String renderTagOpen() {
-            return "";
-        }
-    }
-
-    public abstract static class HtmlComponent {
-        protected final List<HtmlComponent> htmlComponent;
-
-        public HtmlComponent(HtmlComponent... htmlComponent) {
-            this.htmlComponent = List.of(htmlComponent);
-        }
-
-        protected String renderNested() {
-            return htmlComponent.stream()
-                                .map(this::render)
-                                .collect(Collectors.joining());
-        }
-
-        private String render(HtmlComponent component) {
-            return component.render()
-                            .lines()
-                            .map(s -> "    " + s + "\n")
-                            .collect(Collectors.joining());
-        }
-
-        public String render() {
-            return renderTagOpen()
-                   +
-                   renderNested()
-                   +
-                   renderTagClose();
-        }
-
-        protected abstract String renderTagClose();
-
-        protected abstract String renderTagOpen();
     }
 
 }
