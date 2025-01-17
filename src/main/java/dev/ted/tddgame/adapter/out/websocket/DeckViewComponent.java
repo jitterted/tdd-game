1b70061be9dc5f163e58b916a5e8b1e2c354dcd5
Ted M. Young <tedyoung@gmail.com>
2025-01-16 17:02:30 -0800

    Test fails when two (or more) cards in discard pile, we want to show the last (mostly recently discarded) card, not the first card

--- src/main/java/dev/ted/tddgame/adapter/out/websocket/DeckViewComponent.java
+++ src/main/java/dev/ted/tddgame/adapter/out/websocket/DeckViewComponent.java
@@ -3,6 +3,10 @@ package dev.ted.tddgame.adapter.out.websocket;
 import dev.ted.tddgame.adapter.HtmlElement;
 import dev.ted.tddgame.domain.ActionCard;
 import dev.ted.tddgame.domain.DeckView;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+import java.util.function.Function;
 
 public class DeckViewComponent {
     private final DeckView<ActionCard> actionCardDeckView;
@@ -12,22 +16,29 @@ public class DeckViewComponent {
     }
 
     HtmlElement htmlForDiscardAndDrawPiles() {
-        return HtmlElement.forest(drawPile(),
-                                  HtmlElement.swapInnerHtml(
-                                          "action-card-discard-pile"
-                                  ));
+        return HtmlElement.forest(
+                swapForPile(actionCardDeckView.drawPile(),
+                            "action-card-draw-pile",
+                            _ -> HtmlElement.swapInnerHtml("action-card-draw-pile", HtmlElement.img("/action-card-back.png", "Action Card Draw Pile"))),
+                swapForPile(actionCardDeckView.discardPile(),
+                            "action-card-discard-pile",
+                            actionCard -> HtmlElement.swapInnerHtml("action-card-discard-pile", HandViewComponent.imgElementFor(actionCard)))
+        );
     }
 
-    private HtmlElement drawPile() {
-        if (actionCardDeckView.drawPile().isEmpty()) {
-            return HtmlElement.swapInnerHtml(
-                    "action-card-draw-pile");
-        }
+    private HtmlElement swapForPile(List<ActionCard> pile, String targetId, Function<@NotNull ActionCard, HtmlElement> cardToHtmlMapper) {
+        return pile.stream()
+                   .findFirst()
+                   .map(cardToHtmlMapper)
+                   .orElse(HtmlElement.swapInnerHtml(targetId));
 
-        return HtmlElement.swapInnerHtml(
-                "action-card-draw-pile",
-                HtmlElement.img("/action-card-back.png",
-                                "Action Card Draw Pile")
-        );
+//        if (pile.isEmpty()) {
+//            return HtmlElement.swapInnerHtml(targetId);
+//        }
+//
+//        return HtmlElement.swapInnerHtml(
+//                targetId,
+//                imgMapper.apply(pile)
+//        );
     }
 }
--- src/test/java/dev/ted/tddgame/adapter/out/websocket/DeckViewComponentTest.java
+++ src/test/java/dev/ted/tddgame/adapter/out/websocket/DeckViewComponentTest.java
@@ -50,7 +50,41 @@ class DeckViewComponentTest {
                 ));
     }
 
-    // drawPileEmptyAndDiscardPileWithCardHasImageOfCardInDiscardPileSwap
+    @Test
+    void drawPileEmptyAndDiscardPileWithCardHasImageOfCardInDiscardPileSwap() {
+        DeckView<ActionCard> deckView = new DeckView<>(Collections.emptyList(),
+                                                       List.of(ActionCard.PREDICT));
+        DeckViewComponent deckViewComponent = new DeckViewComponent(deckView);
 
+        assertThat(deckViewComponent.htmlForDiscardAndDrawPiles())
+                .isEqualTo(forest(
+                        swapInnerHtml(
+                                "action-card-draw-pile"
+                        ),
+                        swapInnerHtml(
+                                "action-card-discard-pile",
+                                HandViewComponent.imgElementFor(ActionCard.PREDICT)
+                        )
+                ));
+    }
 
+    @Test
+    void drawPileEmptyAndDiscardPileWithTwoCardsHasImageOfLastCardInDiscardPileSwap() {
+        DeckView<ActionCard> deckView = new DeckView<>(Collections.emptyList(),
+                                                       List.of(ActionCard.WRITE_CODE,
+                                                               ActionCard.REFACTOR,
+                                                               ActionCard.LESS_CODE));
+        DeckViewComponent deckViewComponent = new DeckViewComponent(deckView);
+
+        assertThat(deckViewComponent.htmlForDiscardAndDrawPiles())
+                .isEqualTo(forest(
+                        swapInnerHtml(
+                                "action-card-draw-pile"
+                        ),
+                        swapInnerHtml(
+                                "action-card-discard-pile",
+                                HandViewComponent.imgElementFor(ActionCard.LESS_CODE)
+                        )
+                ));
+    }
 }
