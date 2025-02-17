6395836de2e7ed105740d79be038f1c19393ea50
Ted M. Young <tedyoung@gmail.com>
2025-02-17 11:24:30 -0800

    Created TestResultsDeck and update test to use it

--- src/test/java/dev/ted/tddgame/domain/TestResultsDeckTest.java
+++ src/test/java/dev/ted/tddgame/domain/TestResultsDeckTest.java
@@ -2,23 +2,16 @@ package dev.ted.tddgame.domain;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import static org.assertj.core.api.Assertions.*;
 
 class TestResultsDeckTest {
 
     @Test
     void deckHoldsTestResultsCardsThatCanBeDrawn() {
-        Deck<TestResultsCard> testResultsCardDeck = new Deck<>(
-                List.of(
-                        TestResultsCard.NEED_ONE_LESS_CODE,
-                        TestResultsCard.NEED_TWO_LESS_CODE,
-                        TestResultsCard.AS_PREDICTED
-                ),
-                new Deck.IdentityShuffler<>(),
-                new ArrayList<>()
+        TestResultsDeck testResultsCardDeck = TestResultsDeck.createForTest(
+                TestResultsCard.NEED_ONE_LESS_CODE,
+                TestResultsCard.NEED_TWO_LESS_CODE,
+                TestResultsCard.AS_PREDICTED
         );
 
         assertThat(testResultsCardDeck.draw())
@@ -27,8 +20,10 @@ class TestResultsDeckTest {
                 .isEqualByComparingTo(TestResultsCard.NEED_TWO_LESS_CODE);
         assertThat(testResultsCardDeck.draw())
                 .isEqualByComparingTo(TestResultsCard.AS_PREDICTED);
+
         testResultsCardDeck.acceptDiscard(TestResultsCard.NEED_ONE_LESS_CODE);
         assertThat(testResultsCardDeck.draw())
                 .isEqualByComparingTo(TestResultsCard.NEED_ONE_LESS_CODE);
     }
+
 }
